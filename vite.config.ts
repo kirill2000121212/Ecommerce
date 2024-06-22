08bf94d85d4a165d9f977490b726b479c2386c6b
vladick <vladislavtihonov790@gmail.com>
2024-06-22 20:53:23 +0300

    доÐбавил 1 адÐлиас)))

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@components', replacement: path.resolve(__dirname, 'src/components') }
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@public', replacement: path.resolve(__dirname, './public') }
     ]
   }
 })
