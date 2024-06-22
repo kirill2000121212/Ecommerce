97d9bca54ef240bba874a09315d198a1634cbb5e
kirill2000121212 <alakonnex@gmail.com>
2024-06-22 20:15:15 +0300

    начал делать хедер

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,30 @@
+import {Container} from "../container/Container.tsx";
+import logo from  "../../../public/logo.svg"
+import {Link} from "react-router-dom";
+
 export const Header = () => {
     return (
         <header>
-            header
+            <Container>
+                <img src={logo} alt="logo"/>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={'/'}></Link>
+                        </li>
+                        <li>
+                            <Link to={'/'}></Link>
+                        </li>
+                        <li>
+                            <Link to={'/'}></Link>
+                        </li>
+                        <li>
+                            <Link to={'/'}></Link>
+                        </li>
+                    </ul>
+                </nav>
+                <input type="text" placeholder='Search products'/>
+            </Container>
         </header>
     );
 };
