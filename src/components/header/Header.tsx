70a8397e3556a395c944adfc98608bb9aa94744c
kirill2000121212 <alakonnex@gmail.com>
2024-06-22 21:18:13 +0300

    ÑÐначало кастома хедера

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,37 @@
 import {Container} from "../container/Container.tsx";
-import logo from  "../../../public/logo.svg"
+import Logo from  "@public/logo.svg"
 import {Link} from "react-router-dom";
+import Input from '@mui/material/Input';
+import Search from '@/assets/icons/sharedIcons/Search.svg'
+import Cart from '@/assets/icons/sharedIcons/Cart.svg'
+import ProfileUser from '@/assets/icons/sharedIcons/ProfileUser.svg'
 
 export const Header = () => {
     return (
         <header>
             <Container>
-                <img src={logo} alt="logo"/>
+                <img src={Logo} alt="Logo"/>
                 <nav>
                     <ul>
                         <li>
-                            <Link to={'/'}></Link>
+                            <Link to={'/'}>Home</Link>
                         </li>
                         <li>
-                            <Link to={'/'}></Link>
+                            <Link to={'/'}>Categories</Link>
                         </li>
                         <li>
-                            <Link to={'/'}></Link>
+                            <Link to={'/'}>About</Link>
                         </li>
                         <li>
-                            <Link to={'/'}></Link>
+                            <Link to={'/'}>Contact</Link>
                         </li>
                     </ul>
                 </nav>
-                <input type="text" placeholder='Search products'/>
+                <Input placeholder='Search products' startAdornment={
+                    <img src={Search} alt="Search"/>
+                }/>
+                <button><img src={Cart} alt="Cart"/></button>
+                <button><img src={ProfileUser} alt="ProfileUser"/></button>
             </Container>
         </header>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@public', replacement: path.resolve(__dirname, './public') }
+    ]
+  }
 })
